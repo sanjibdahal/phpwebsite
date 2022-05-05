563d86b17af2e151f4c4dfe6a5d59963fcd61674
sanjibdahal <90917889+sanjibdahal@users.noreply.github.com>
2022-05-05 19:30:26 +0545

    Update links.php

--- links.php
+++ links.php
@@ -6,11 +6,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-<link rel="stylesheet" href="all.css">
-<link rel="stylesheet" href="bootstrap.min.css">
 <link rel="stylesheet" href="all.min.css">
-<link rel="stylesheet" href="css2.css">
-<script src="bootstrap.js"></script>
 
 <!-- Optional JavaScript; choose one of the two! -->
 <script src="https://kit.fontawesome.com/034dd42c35.js" crossorigin="anonymous"></script>
